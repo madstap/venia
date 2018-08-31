661416f0753fb750e15de2354bd8b9da3f1b129e
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2018-08-31 03:08:46 -0300

    temporary fork

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vincit/venia "0.2.6-SNAPSHOT"
+(defproject madstap/venia "0.2.6"
   :description "Clojure(Script) graphql client"
   :url "https://github.com/Vincit/venia"
   :license {:name "Eclipse Public License"
